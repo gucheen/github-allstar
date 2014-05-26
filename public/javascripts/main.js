39c5add0b3f1645198c1d2a4c30f31ddd5be9bb0
gucheen <guchengf@gmail.com>
2014-05-26 13:13:21 +0800

    Add checking steps;

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,8 @@
 $(function() {
     var va_code = getQueryStringRegExp('code');
     var access_token = docCookies.getItem('access_token');
+    var repos = $('#repo-list');
+    var progress_bar = $('.progress-bar');
     if (va_code && !access_token) {
         $.post('/access', {
             code: va_code
@@ -16,7 +18,11 @@ $(function() {
         e.preventDefault();
         var tar_user = $('#tar-user').val();
         if (tar_user) {
-            $.get('https://api.github.com/users/' + tar_user + '/starred', function(resp) {
+            if (repos.length) {
+                $('#repo-list').empty();
+                $('.progress-bar').width('0');
+            }
+            $.get('https://api.github.com/users/' + tar_user + '/starred?per_page=100', function(resp) {
                 var add_repo_list = [];
                 var repo_collection = [];
                 for (var i = 0; i < resp.length; i++) {
@@ -25,7 +31,7 @@ $(function() {
                     add_repo_list.push(add_repo);
                 }
                 $('#repo-list').append(add_repo_list);
-                $('.progress-bar').width('5%');
+                progress_bar.width('5%');
             });
         } else {
             $('#tar-user').focus();
@@ -33,20 +39,27 @@ $(function() {
     });
     $('#run').click(function(e) {
         e.preventDefault();
-        $('.repo-check:checked').each(function(i, e) {
-            var $e = $(e);
-            var rp = $e.data('repo');
-            $.post('/star', {
-                repo: rp,
-                accessToken: access_token
-            }, function(resp) {
-                console.log(resp);
-                if (resp === '204') {
-                    $e.parent().children('a').css('color', '#27ae60');
-                } else {
-                    $e.parent().children('a').css('color', '#c0392b');
-                }
+        if (repos.length) {
+            $('.repo-check:checked').each(function(i, e) {
+                var $e = $(e);
+                var rp = $e.data('repo');
+                $.post('/star', {
+                    repo: rp,
+                    accessToken: access_token
+                }, function(resp) {
+                    console.log(resp);
+                    if (resp === '204') {
+                        $e.parent().children('a').css('color', '#27ae60');
+                    } else {
+                        $e.parent().children('a').css('color', '#c0392b');
+                    }
+                });
             });
-        });
+        }
+    });
+    $('#reset').click(function(e) {
+        e.preventDefault();
+        repos.empty();
+        progress_bar.width('0');
     });
 });
